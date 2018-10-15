3512a10c9e240b7aa1873b8ab34479abd5f9f6ae
alexey bokov <bokov@bokov.net>
2018-10-15 09:56:12 -0700

    Update mount.sh
    Mount all drives from fdisk into /mnt

--- mount.sh
+++ mount.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo test >> test.txt  # todo: add real mount script
+MY_MOUNT_DIR="/mnt"
+
+for MY_PARTITION in `fdisk -l | grep -v swap | egrep -o /dev/sd.[0-9]+` ; do
+    mount | grep -q "^$MY_PARTITION"
+    if [ $? -eq 1 ] ; then
+          blkid $MY_PARTITION
+          if [ $? -eq 0 ] ; then
+              MOUNTPOINT=$MY_MOUNT_DIR/`echo $MY_PARTITION | egrep -o sd.[0-9]+`
+              mkdir -p $MOUNTPOINT
+              mount $MY_PARTITION $MOUNTPOINT
+          fi
+fi
+
+done
